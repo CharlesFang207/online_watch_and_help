2060711be826f1012c98fc9bf90ba96f525e2f88
Xinyu Fang <xfang21@jh.edu>
2024-05-12 15:52:23 -0400

    change directory of the data generation code

--- gen_data/vh_init_structured_old_for_2.3.py
+++ gen_data/vh_init_structured_old_for_2.3.py
@@ -11,7 +11,7 @@ import argparse
 
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 home_path = "../../"
-sys.path.insert(0, f"{curr_dir}/../../virtualhome/")
+sys.path.insert(0, f"{curr_dir}/../../virtualhome/virtualhome/")
 
 print("path", sys.path[0])
 
@@ -50,7 +50,7 @@ parser.add_argument(
 parser.add_argument(
     "--exec_file",
     type=str,
-    default="//data/vision/torralba/frames/data_acquisition/SyntheticStories/website/release/simulator/v2.0/v2.3.0/aviv/linux_exec.v2.3.0.x86_64",
+    default="/home/scai/Workspace/xfang21/github_repos/linux_exec.v2.3.0.x86_64",
     help="Use unity editor",
 )
 
